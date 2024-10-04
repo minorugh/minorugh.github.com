e1dededc9bd4deed4f64a1b2b358c5575c863488
minorugh <minorugh@gmail.com>
2024-10-04 11:36:28 +0900

    Updated: 2024-10-04 11:36:28

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm -r docs
-hugo
 git add . && git diff --cached --exit-code --quiet && echo "\nnothing to commit, working tree clean!"|| \
 git commit -a -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`" && \
 git push origin main
